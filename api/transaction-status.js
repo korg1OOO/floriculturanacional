184ed9fa5ef6152030b6e1e22db952161f164444
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2025-06-11 01:16:10 -0300

    Update transaction-status.js

--- api/transaction-status.js
+++ api/transaction-status.js
@@ -1,1 +1,15 @@
+const { getTransactionStatus } = require('./webhooks');
 
+module.exports = async (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
+  const { id } = req.query;
+  if (!id) {
+    return res.status(400).json({ message: 'Transaction ID is required' });
+  }
+
+  const status = getTransactionStatus(id);
+  res.status(200).json(status);
+};
